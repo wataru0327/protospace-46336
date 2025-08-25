7b3bfb51235cbab802cdf232479da6c7216ee652
wataru0327 <watarurataw00@icloud.com>
2025-08-25 20:35:37 +0900

    コメント投稿機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-
   root to: "prototypes#index"
-
 
   devise_for :users
 
-
   resources :prototypes do
-    resources :comments, only: :create 
- 
+    resources :comments, only: :create
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
