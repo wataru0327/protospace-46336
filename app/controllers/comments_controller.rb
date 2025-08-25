fe4d38cc3284ea8698e592b1cca4ed46c94fde7b
wataru0327 <watarurataw00@icloud.com>
2025-08-25 20:04:41 +0900

    コントローラーを作成後アクションとルーティングを設定

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,7 @@
+class CommentsController < ApplicationController
+end
+
+class CommentsController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   devise_for :users
 
 
-  resources :prototypes
+  resources :prototypes do
+    resources :comments, only: :create 
  
   get "up" => "rails/health#show", as: :rails_health_check
 end
