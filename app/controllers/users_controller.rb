e683515bf7374da6007405bf76132552dcfa2bce
wataru0327 <watarurataw00@icloud.com>
2025-08-25 20:44:51 +0900

    ユーザーコントロール作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def show
+    # ユーザー詳細ページを表示する処理は後で書く
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     resources :comments, only: :create
   end
 
+  resources :users, only: :show
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
