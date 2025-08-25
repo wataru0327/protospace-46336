672c7dffd21abafe8b7e60c5ef2885a09edd2ad6
wataru0327 <watarurataw00@icloud.com>
2025-08-25 12:18:03 +0900

    Prototypeモデル作成後バリデーション設定

--- Gemfile
+++ Gemfile
@@ -68,3 +68,6 @@ group :test do
 end
 
 gem 'devise'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  has_many :prototypes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+ 
   validates :name, presence: true
   validates :profile, presence: true
   validates :occupation, presence: true
