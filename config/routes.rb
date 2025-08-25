3a77cd220e5a8b51221fd2e02753712dec0a1829
wataru0327 <watarurataw00@icloud.com>
2025-08-25 12:32:49 +0900

    prototypesnewアクションとcreateアクションを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
+  root to: "prototypes#index"
+
+
   devise_for :users
-  root "prototypes#index"
 
+
+  resources :prototypes, only: [:new, :create, :index]
+
+ 
   get "up" => "rails/health#show", as: :rails_health_check
-
-
 end
